aeb923d362ab92c753b6975a57d23e91657bfa2a
fizzaalwani <fizzaokok@gmail.com>
2025-07-17 23:26:54 +0500

    cart functionality completed with css

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-// let arr=[1,2,3,4,5,6,7,8,9,10];
-// console.log(arr.includes(2))
-// let arr2=arr.filter((item,index)=>item%2===0
-// )
-// console.log(arr2); // Output: [2, 4, 6, 8, 10]
+let arr=[1,2,3,4,5,6,7,8,9,10];
+console.log(arr.includes(2))
+let arr2=arr.filter((item,index)=>item%2===0
+)
+console.log(arr2); // Output: [2, 4, 6, 8, 10]
 // let arr3=arr2.splice(0,3)
 // console.log(arr3); // Output: [10]
 let data=[];
-const favourites=[];
-const cart=[];
-
+// const favourites=[];
+// const cart=[];
 
 
 (async function getData(){
@@ -17,6 +16,8 @@ const cart=[];
         data=await res.json();
         console.log(data);
        renderProducts(data);
+        renderCartPage();
+         removeItem();
 
        
     }catch(err){
@@ -27,7 +28,8 @@ const cart=[];
 
 const renderProducts=(data)=>{
     const productContainer=document.querySelector('.products');
-    productContainer.innerHTML=''
+    if(productContainer){
+           productContainer.innerHTML=''
     data.forEach((item)=>{
         const product=document.createElement('div');
         product.classList.add('product');
@@ -53,15 +55,16 @@ const renderProducts=(data)=>{
           <div class="p-price" style="font-weight: 600;">$${item.price}</div>`
            productContainer.appendChild(product)
     })
-    
+    }
 }
+
 (function filterDataCategory(){
 let categories=document.querySelectorAll('.category-container li')
 console.log(categories)
 
 categories.forEach((category,index)=>{
     category.addEventListener('click',()=>{
-        console.dir(category.outerText)
+        // console.dir(category.outerText)
 
        if(category.outerText=="Home"){
           renderProducts(data)
@@ -70,7 +73,7 @@ categories.forEach((category,index)=>{
           let filteredData=data.filter((data,index)=>{
            return data.category.trim().toLowerCase()==category.outerText.trim().toLowerCase()
         })
-        console.log(filteredData)
+        // console.log(filteredData)
         renderProducts(filteredData)
     } 
        
@@ -79,6 +82,7 @@ categories.forEach((category,index)=>{
 })();
 
 let product=document.querySelector('.products')
+if(product){
 product.addEventListener('click',(e)=>{
     if(e.target.tagName!== 'I')
         return
@@ -104,8 +108,14 @@ product.addEventListener('click',(e)=>{
     }
    
 })
+}
+
+
+
 
 function addToCart(productTitle){
+
+    let cart=getCart();
     
     existingProduct=cart.find((item)=> item.productTitle===productTitle)
     console.log(existingProduct)
@@ -116,11 +126,14 @@ function addToCart(productTitle){
          let product={productTitle,quantity:1}
          cart.push(product)
     }
-      renderCartIcon(cart.length)
+    localStorage.setItem("cart",JSON.stringify(cart))
+     renderIcon("cart",'.cart-count')
 }
 
 function addToFavourite(productTitle){
-       existingProduct=cart.find((item)=> item.productTitle===productTitle)
+
+    let favourites=getFavourites();
+       existingProduct=favourites.find((item)=> item.productTitle===productTitle)
     console.log(existingProduct)
 
     if(existingProduct){
@@ -130,14 +143,20 @@ function addToFavourite(productTitle){
         let product={productTitle,quantity:1}
         favourites.push(product)
     }
-    renderHeartIcon(favourites.length)
+    localStorage.setItem("favourites",JSON.stringify(favourites))
+    renderIcon("favourites",'.heart-count')
 }
-function renderCartIcon(count){
-    document.querySelector('.cart-count').innerText=count
+
+
+function renderIcon(key,selector){
+    let items=JSON.parse(localStorage.getItem(key)) || []
+    console.log(items)
+    document.querySelector(selector).innerText=items.length
 }
-function renderHeartIcon(count){
-    document.querySelector('.heart-count').innerText=count
-}
+
+renderIcon("cart",'.cart-count');
+renderIcon("favourites",'.heart-count')
+
 function displayProductShort(productTitle){
 
     // product.length=0;
@@ -155,7 +174,88 @@ function displayProductShort(productTitle){
 function closeSlide(){
     document.querySelector('.shortCut').classList.toggle('hidden')
 }
+function renderCartPage(){
+    let subtotal=0;
+    let i=0;
+    let cart=getCart();
+    const tableBody= document.querySelector('.table-body');
+    if(!tableBody) return 
+    tableBody.innerHTML=''
+    console.log("Added to cart items :",cart)
 
+    cart.forEach((cart)=>{
+        const product=data.find((product)=> product.title.trim().toLowerCase()===cart.productTitle.trim().toLowerCase())
+        if(product){
+           
+           i+=1;
+           tableBody.innerHTML+=`
+             <tr class='productDetails'>
+        <td>${i}</td>
+        <td><img src=${product.image} alt="Ring"></td>
+        <td>${product.title}</td>
+        <td>${product.price}</td>
+        <td>${cart.quantity}</td>
+        <td>$${cart.quantity*product.price}</td>
+        <td><button class="remove-btn"><i class="fa-solid fa-xmark"></i></button></td>
+      </tr>
+           `
+           subtotal+=cart.quantity*product.price
+        }
+    })
+
+    document.querySelector('.subtotal').innerText=`$ ${subtotal}`
+    document.querySelector('.total-price').innerText=`$ ${subtotal}`
+    // removeItem();
+};
+
+
+
+function removeItem(){
+    
+    let tableBody=document.querySelector('.table-body')
+    tableBody.addEventListener('click',(e)=>{
+        if(e.target.tagName!=='I')
+            return;
+    
+
+        let parent=e.target.closest('.productDetails')
+        if (!parent) return;
+        let productTitle=parent.children[2].innerText;
+        // let quantity=parent.children[4].innerText;
+
+
+        let cart=getCart();
+        cart=cart.filter((item)=>{
+            return item.productTitle.trim().toLowerCase()!==productTitle.trim().toLowerCase()
+        })
+        console.log("Updated cart : ",cart)
+        localStorage.setItem("cart",JSON.stringify(cart))
+
+        renderCartPage();
+        renderIcon("cart",'.cart-count');
+    })
+}
+
+//getters
+function getCart(){
+    return JSON.parse(localStorage.getItem("cart")) || []
+}
+function getFavourites(){
+    return JSON.parse(localStorage.getItem("favourites")) || []
+}
+
+function clearCart(){
+    localStorage.removeItem("cart");
+    let cart=getCart();
+    console.log(cart)
+}
+
+
+document.querySelector('.clear-cart-btn').addEventListener('click',()=>{
+    clearCart();
+    renderCartPage();
+    renderIcon('cart','.heart-count')
+})
 
 
 
